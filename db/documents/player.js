57c89c0d55d3375c9cee1f5ba3d500885d9eac1e
rouquetr <raphael.rouquet@utt.fr>
2017-06-05 22:57:27 +0200

    [Player] changed reward from string to number

--- db/documents/player.js
+++ db/documents/player.js
@@ -2,7 +2,7 @@ function createPlayerSchema (mongoose, name = 'Player') {
   const PlayerSchema = new mongoose.Schema({
     player: { name: String, googleId: String, reputation: Number, money: Number },
     characters: [ { name: String, class: String, weaponLevel: Number, armorLevel: Number } ],
-    availableQuests: { generatedAt: Date, Quests: [ { level: Number, length: Date, lethality: Number, reward: String } ] } //TODO: désigner clairement reward
+    availableQuests: { generatedAt: Date, Quests: [ { level: Number, length: Date, lethality: Number, reward: Number } ] }
   }, { minimize: false, strict: true })
   return mongoose.model('Player', PlayerSchema)
 }
