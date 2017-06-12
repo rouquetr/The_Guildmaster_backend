a348680b8e0bc825db065a162be12929eae6bf4d
rouquetr <raphael.rouquet@utt.fr>
2017-06-12 11:19:11 +0200

    [Player] added id to the player model for characters and quests

--- db/documents/Player.js
+++ db/documents/Player.js
@@ -1,10 +1,10 @@
 function createPlayerSchema (mongoose, name = 'Player') {
   const PlayerSchema = new mongoose.Schema({
     player: { name: String, googleId: String, reputation: Number, money: Number },
-    characters: [ { name: String, class: String, weaponLevel: Number, armorLevel: Number } ],
+    characters: [ { id: String, name: String, class: String, weaponLevel: Number, armorLevel: Number } ],
     quests: {
-      availableQuests: { generatedAt: Date, quests: [ { questType: String, level: Number, length: Number, lethality: Number, reward: Number } ] },
-      currentQuests: [ { endAt: Date, questType: String, level: Number, length: Number, lethality: Number, reward: Number } ]
+      availableQuests: { generatedAt: Date, quests: [ { id: String, questType: String, level: Number, length: Number, lethality: Number, reward: Number } ] },
+      currentQuests: [ { id: String, endAt: Date, questType: String, level: Number, length: Number, lethality: Number, reward: Number } ]
     }
   }, { minimize: false, strict: true })
   return mongoose.model('Player', PlayerSchema)
